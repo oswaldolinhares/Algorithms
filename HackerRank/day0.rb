f75e90042fb70ca0f5484a4d31710a52bce42985
Oswaldo Linhares <oswaldolinhares@gmail.com>
2020-02-04 19:18:21 -0300

    Day 0

---
+++ HackerRank/day0.rb
@@ -0,0 +1,9 @@
+#Ler informação do teclado e colocar na variável inputString
+#r
+inputString = gets.chomp
+
+#Imprime a Primeira Linha
+puts 'Hello, World.'
+
+#Imprime a Segunda Linha
+puts inputString
